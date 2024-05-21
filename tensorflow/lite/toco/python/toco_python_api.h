raavanans/tensorflow

Adding MLIR debugging instrumentation to experimental MLIR Quantizer
PiperOrigin-RevId: 623867514

tensorflow/lite/toco/python/toco_python_api.h
@@ -53,7 +53,8 @@ PyObject* MlirQuantizeModel(PyObject* data, bool disable_per_channel,
                             PyObject* op_denylist = nullptr,
                             PyObject* node_denylist = nullptr,
                             bool enable_variable_quantization = false,
-                            bool disable_per_channel_for_dense_layers = false);
+                            bool disable_per_channel_for_dense_layers = false,
+                            PyObject* toco_flags_proto_txt_raw = nullptr);
 
 // Sparsifies model to encode sparse tensors with proper format. Throws error if
 // sparsification fails.
@@ -63,7 +64,7 @@ PyObject* MlirSparsifyModel(PyObject* data);
 PyObject* RegisterCustomOpdefs(PyObject* list);
 
 // Returns the collected TFLite conversion errors.
-const std::vector<std::string> RetrieveCollectedErrors();
+std::vector<std::string> RetrieveCollectedErrors();
 
 // Returns MLIR string dump of the given Flatbuffer model.
 std::string FlatBufferFileToMlir(const std::string& model,

tensorflow/python/lite/toco_python_api_wrapper.cc
@@ -61,14 +61,15 @@ PYBIND11_MODULE(_pywrap_toco_api, m) {
          int output_data_type, bool enable_numeric_verify,
          bool enable_whole_model_verify, py::object op_blocklist,
          py::object node_blocklist, bool enable_variable_quantization,
-         bool disable_per_channel_for_dense_layers) {
+         bool disable_per_channel_for_dense_layers,
+         py::object toco_flags_proto_txt_raw) {
         return tensorflow::PyoOrThrow(toco::MlirQuantizeModel(
             input_contents_txt_raw.ptr(), disable_per_channel, fully_quantize,
             inference_type, input_data_type, output_data_type,
             enable_numeric_verify, enable_whole_model_verify,
             op_blocklist.ptr(), node_blocklist.ptr(),
-            enable_variable_quantization,
-            disable_per_channel_for_dense_layers));
+            enable_variable_quantization, disable_per_channel_for_dense_layers,
+            toco_flags_proto_txt_raw.ptr()));
       },
       py::arg("input_contents_txt_raw"), py::arg("disable_per_channel") = false,
       py::arg("fully_quantize") = true, py::arg("inference_type") = 9,
@@ -79,6 +80,7 @@ PYBIND11_MODULE(_pywrap_toco_api, m) {
       py::arg("node_blocklist") = py::none(),
       py::arg("enable_variable_quantization") = false,
       py::arg("disable_per_channel_for_dense_layers") = false,
+      py::arg("toco_flags_proto_txt_raw") = nullptr,
       R"pbdoc(
       Returns a quantized model.
     )pbdoc");

psmman/TNFlow

tensorflow/lite/toco/python/toco_python_api.h
@@ -53,7 +53,8 @@ PyObject* MlirQuantizeModel(PyObject* data, bool disable_per_channel,
                             PyObject* op_denylist = nullptr,
                             PyObject* node_denylist = nullptr,
                             bool enable_variable_quantization = false,
-                            bool disable_per_channel_for_dense_layers = false);
+                            bool disable_per_channel_for_dense_layers = false,
+                            PyObject* debug_options_proto_txt_raw = nullptr);
 
 // Sparsifies model to encode sparse tensors with proper format. Throws error if
 // sparsification fails.
@@ -63,7 +64,7 @@ PyObject* MlirSparsifyModel(PyObject* data);
 PyObject* RegisterCustomOpdefs(PyObject* list);
 
 // Returns the collected TFLite conversion errors.
-const std::vector<std::string> RetrieveCollectedErrors();
+std::vector<std::string> RetrieveCollectedErrors();
 
 // Returns MLIR string dump of the given Flatbuffer model.
 std::string FlatBufferFileToMlir(const std::string& model,

tensorflow/python/lite/toco_python_api_wrapper.cc
@@ -61,14 +61,15 @@ PYBIND11_MODULE(_pywrap_toco_api, m) {
          int output_data_type, bool enable_numeric_verify,
          bool enable_whole_model_verify, py::object op_blocklist,
          py::object node_blocklist, bool enable_variable_quantization,
-         bool disable_per_channel_for_dense_layers) {
+         bool disable_per_channel_for_dense_layers,
+         py::object debug_options_proto_txt_raw) {
         return tensorflow::PyoOrThrow(toco::MlirQuantizeModel(
             input_contents_txt_raw.ptr(), disable_per_channel, fully_quantize,
             inference_type, input_data_type, output_data_type,
             enable_numeric_verify, enable_whole_model_verify,
             op_blocklist.ptr(), node_blocklist.ptr(),
-            enable_variable_quantization,
-            disable_per_channel_for_dense_layers));
+            enable_variable_quantization, disable_per_channel_for_dense_layers,
+            debug_options_proto_txt_raw.ptr()));
       },
       py::arg("input_contents_txt_raw"), py::arg("disable_per_channel") = false,
       py::arg("fully_quantize") = true, py::arg("inference_type") = 9,
@@ -79,6 +80,7 @@ PYBIND11_MODULE(_pywrap_toco_api, m) {
       py::arg("node_blocklist") = py::none(),
       py::arg("enable_variable_quantization") = false,
       py::arg("disable_per_channel_for_dense_layers") = false,
+      py::arg("debug_options_proto_txt_raw") = nullptr,
       R"pbdoc(
       Returns a quantized model.
     )pbdoc");